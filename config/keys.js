4ca40fdf472927065468b9d8c276cc5d7706ca0d
rahulsingh2711 <singhrahulkumar564@gmail.com>
2021-02-17 16:19:32 +0530

    v 1.0.1

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://rahul:rahul123@cluster1.xvh9h.mongodb.net/mern_shopping?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://<username>:<password>@cluster1.xvh9h.mongodb.net/<collection_name>?retryWrites=true&w=majority'
 
 }
